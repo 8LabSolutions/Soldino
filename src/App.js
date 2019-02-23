f9af6a0a047e07fa1bcb86f91364ecbe62f279b4
Samuele <samuele.piazzetta@gmail.com>
2019-02-23 18:14:06 +0100

    Settate un po' di cose

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 import React, { Component } from 'react';
-import Todos from './todos'
-import AddTodo from './AddTodo'
+import './style/App.css';
+
 class App extends Component {
-  state = {
-    todos : [
-      {id: 1, content: 'one'},
-      {id: 2, content: 'two'}
-    ]
-  }
-
-deleteTodo = (id) =>{
-  const todos = this.state.todos.filter((todo) => {
-    return todo.id !== id
-  });
-  this.setState({
-    todos
-  })
-}
-
-addTodo = (todo) => {
-  todo.id = Math.random();
-  //non posso modificare l'altro
-  let todos = [...this.state.todos, todo];
-  this.setState({
-    todos
-  })
-}
-
   render() {
     return (
-      <div className="todo-app container">
-        <h1 className="center blue-text">Francesco merda (e anche Greggio)</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}  />
-        <AddTodo addTodo={this.addTodo} />
+      <div className="App">
+        Esempio
       </div>
     );
   }
