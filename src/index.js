0178a88cf93c9214f3a48a67addc3c91a7a8c19c
Francesco Donè <francesco.done.11597@gmail.com>
2019-03-19 17:49:35 +0100

    Added css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from "./store/index";
 import App from './App';
+import './styles/style.css' 
 
 ReactDOM.render(
   <Provider store={store}>
