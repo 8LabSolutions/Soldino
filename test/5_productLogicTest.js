3369b9b516c0ce94cf37c315d2970d0e5bcf1e29
Matteo <matteo.santinon12@gmail.com>
2019-04-09 17:32:51 +0200

    new tests UL corretti PL

--- test/5_productLogicTest.js
+++ test/5_productLogicTest.js
@@ -4,6 +4,7 @@ const { getWeb3 } = require('./helpers')
 const ContractManager = artifacts.require("ContractManager");
 const ProductStorage = artifacts.require("ProductStorage");
 const ProductLogic = artifacts.require("ProductLogic");
+const UserLogic = artifacts.require("UserLogic");
 
 var web3 = getWeb3()
 
@@ -11,6 +12,8 @@ contract("ProductLogic", (accounts) => {
     var contractManagerInstance;
     var productStorageInstance;
     var productLogicInstance;
+    var userLogicInstance;
+    
     before(() => {
         contractManagerInstance = new web3.eth.Contract(
           ContractManager.abi,
@@ -26,9 +29,11 @@ contract("ProductLogic", (accounts) => {
           ProductLogic.abi,
           ProductLogic.networks[ContractManager.network_id].address
         );
-    
-        productStorageInstance.methods.owner().call().then((result) => {console.log(result)})
-    
+
+        userLogicInstance = new web3.eth.Contract(
+          UserLogic.abi,
+          UserLogic.networks[ContractManager.network_id].address
+        );
       });
 
       it("should check if product logic is added to the authorized contract of ProductStorage", () => {
@@ -50,39 +55,43 @@ contract("ProductLogic", (accounts) => {
         })
       });
 
-      it("should add a new product", () => {
+      it("should add a new product successfully", () => {
         var key = "0x7465737410000000000000000000000000000000000000000000000000000000"
         var size = 2
         var funH = 3
         var vat = 22
         var price = 100
-
-        return productLogicInstance.methods.addProduct(
-          key,
+        return userLogicInstance.methods.addBusiness(
+          accounts[4],
           size,
-          funH,
-          vat,
-          price
+          funH
         ).send({from: accounts[4], gas:2000000})
-        .then(() => {
-          
-          /*return truffleAssert.eventEmitted(result, 'ProductInserted', (ev) => {
-            return ev._keyHash == "0x7465737410000000000000000000000000000000000000000000000000000000"
-            && ev._seller == accounts[4];
-          })*/
-          return productStorageInstance.methods.getProduct(key).call()
-      .then((result) => {
-        assert.equal(result[0],key);
-        assert.equal(result[1],size);
-        assert.equal(result[2],funH);
-        assert.equal(result[3],vat);
-        assert.equal(result[4],price);
-        assert.equal(result[5],accounts[4]);
+        .then(()=>{
+          return productLogicInstance.methods.addProduct(
+            key,
+            size,
+            funH,
+            vat,
+            price
+          ).send({from: accounts[4], gas:2000000})
+          .then(() => {
+            
+            /*return truffleAssert.eventEmitted(result, 'ProductInserted', (ev) => {
+              return ev._keyHash == "0x7465737410000000000000000000000000000000000000000000000000000000"
+              && ev._seller == accounts[4];
+            })*/
+            return productStorageInstance.methods.getProduct(key).call()
+            .then((result) => {
+              assert.equal(result[0],key);
+              assert.equal(result[1],size);
+              assert.equal(result[2],funH);
+              assert.equal(result[3],vat);
+              assert.equal(result[4],price);
+              assert.equal(result[5],accounts[4]);
+            })
           })
-
-
         })
-
+        
       });
 
       it("should modify a product", () => {
