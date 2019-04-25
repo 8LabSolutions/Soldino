9b2614326b13f9c95211d236e1ad548b537b53e0
Francesco Donè <francesco.done.11597@gmail.com>
2019-04-25 16:29:19 +0200

    Changed footer text

--- src/components/presentational/Footer.js
+++ src/components/presentational/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
   render() {
     return (
       <footer id="footer" className="page-footer font-small blue pt-4">
-        <div className="footer-copyright text-center py-3">© 2019 Copyright:<a href="http://redbabel.com/"> RedBabel.</a></div>
+        <div className="footer-copyright text-center py-3">2019 Designed and developed by <a href="https://github.com/8LabSolutions">8Lab Solutions</a> for <a href="http://redbabel.com/"> RedBabel.</a></div>
       </footer>
     )
   }
