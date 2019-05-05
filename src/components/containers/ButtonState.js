99d352c66620fd263c172ceab6a06005d863d413
Francesco Donè <francesco.done.11597@gmail.com>
2019-05-05 10:57:42 +0200

    Continue commenting containers from top to bottom pt.2

--- src/components/containers/ButtonState.js
+++ src/components/containers/ButtonState.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { connect } from 'react-redux';
 import { withToastManager } from 'react-toast-notifications';
 import Button from '../presentational/Button';
@@ -6,28 +5,41 @@ import governmentActionCreator from "../../actionsCreator/governmentActionCreato
 import history from '../../store/history'
 import { CITIZEN } from '../../constants/actionTypes';
 import { beginLoading, endLoading } from '../../actions/login';
+import { ERRORTOAST, SUCCESSTOAST } from '../../constants/fixedValues';
 
+/**
+ * @description map the changeState action into the Button component
+ * @param {*} dispatch
+ * @param {*} ownProps
+ */
 const mapDispatchToProps = (dispatch, ownProps) => {
   const { toastManager } = ownProps;
   let redirectUrl
   (ownProps.type===CITIZEN) ? redirectUrl="/userslist" : redirectUrl="/businesslist"
   return {
+    /**
+     * @description change the state of a specific user
+     */
     action: () => {
       dispatch(beginLoading())
       governmentActionCreator.changeUserState(ownProps.address, ownProps.state, ownProps.type)
       .then((action)=>{
         dispatch(action)
+        toastManager.add("User state changed successfully.", SUCCESSTOAST)
         dispatch(endLoading())
         history.push(redirectUrl)
       })
       .catch((err)=>{
-        console.log(err)
+        toastManager.add(err, ERRORTOAST)
         dispatch(endLoading())
       })
     }
   }
 }
 
+/**
+ * @description connect the action to the Button props
+ */
 const ButtonState = connect(null, mapDispatchToProps)(Button);
 
 export default withToastManager(ButtonState);
