5076430877abbe7bf42524046603b76a12067a18
Francesco Donè <francesco.done.11597@gmail.com>
2019-05-10 21:56:36 +0200

    Fixed logout message

--- src/components/containers/ButtonAuth.js
+++ src/components/containers/ButtonAuth.js
@@ -27,16 +27,12 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   const { toastManager } = ownProps;
   return {
     action: () => {
-      let id;
+      let id = null;
       let id2 = null;
-      let messageInfo = "IPFS' connection may take a while, please wait until you are logged ";
       if(store.getState().logged === false){
         //logged out
-        toastManager.add(messageInfo+"in.", INFOTOAST, (x)=>{id=x});
+        toastManager.add("IPFS' connection may take a while, please wait until you are logged in.", INFOTOAST, (x)=>{id=x});
         toastManager.add(didYouKnowThat(), DIDYOUKNOWTOAST, (x)=>{id2=x})
-      }else{
-        //logged in
-        toastManager.add(messageInfo+"out.", INFOTOAST, (x)=>{id=x});
       }
       //get the object if user exists
       authentication.userLogin()
@@ -51,13 +47,13 @@ const mapDispatchToProps = (dispatch, ownProps) => {
           dispatch(logOut())
           history.push('/')
         })
-        toastManager.remove(id);
+        if(id !== null) toastManager.remove(id);
         if(id2 !== null) toastManager.remove(id2)
         toastManager.add(message, SUCCESSTOAST);
         history.push('/')
       })
       .catch((error) => {
-        toastManager.remove(id);
+        if(id !== null) toastManager.remove(id);
         if(id2 !== null) toastManager.remove(id2)
         toastManager.add(error, ERRORTOAST);
       })
