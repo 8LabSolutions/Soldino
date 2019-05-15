6dcd1022404f7e32bf352629e0b35b891f9c3173
Francesco Donè <francesco.done.11597@gmail.com>
2019-05-15 13:30:18 +0200

    Removed redux view with chrome extension

--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,5 @@ import { persistStore, persistReducer } from 'redux-persist';
 import { persistConfig, rootReducer } from '../reducers/index';
 
 const pReducer = persistReducer(persistConfig, rootReducer);
-export const store = createStore(pReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(pReducer);
 export const persistor = persistStore(store);
